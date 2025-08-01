f9dc9a022482e72962a048461bed0e730562f1a0
Aditya <aditya11.hb@gmail.com>
2025-08-01 12:44:12 +0530

    added response interceptor for auto logout in token expiry

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,36 +1,34 @@
 import { config } from '@/config/environment';
 import axios from 'axios';
+import {toast} from "react-toastify";
 
 const axiosInstance = axios.create({
-  baseURL: config.baseURL,
+    baseURL: config.baseURL,
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = sessionStorage.getItem('authToken');
-    console.log(sessionStorage.getItem('authToken'))
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    (config) => {
+        const token = sessionStorage.getItem('authToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//         const status = error?.response?.status;
-//         if (status === 401 || status === 403) {
-//             sessionStorage.removeItem('authToken');
-//             sessionStorage.clear();
-//             setTimeout(() => {
-//                 alert("Your session has expired. Please login again.");
-//                 window.location.replace('/');
-//             }, 300);
-//         }
-//         return Promise.reject(error);
-//     }
-// );
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const status = error?.response?.status;
+        if (status === 401 || status === 403) {
+            sessionStorage.clear();
+            localStorage.clear();
+            toast.info("Session Expired! Please Login Again")
+            window.location.replace('/');
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
